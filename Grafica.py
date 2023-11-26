a7841e8f11bf499e26dcf04b9d58be38979310ba
kused <erreapesrk@gmail.com>
2023-11-26 17:31:58 -0600

    2 graficas funcionales y 2 slider para el control de los leds, final de etapa

--- Grafica.py
+++ Grafica.py
@@ -5,6 +5,13 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtCore import QTimer, Qt
 from ComunicacionSerial import ComunicacionSerial
 
+crimson = '#DC143C'
+seaGreen = '#2E8B57'
+
+steelBlue = '#4682B4'
+slateGray = '#708090'
+violetRed = '#DB7093'
+
 des = '#7FFFD4'
 con = '#CD5C5C'
 
@@ -14,41 +21,66 @@ class Grafica(QWidget):
         self.comunicacion = ComunicacionSerial("/dev/ttyACM0", 9600)
         self.comunicacion.conectar()
 
-        self.layout = QVBoxLayout(self)
+        self.layout_principal = QVBoxLayout(self)
         self.inicializar_ui()
 
         self.conectado = False
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.actualizar_graficas)
-        self.timer_envio_datos = QTimer(self)
-        self.timer_envio_datos.timeout.connect(self.enviar_datos_desde_timer)
+        self.timer_envio_datosA = QTimer(self)
+        self.timer_envio_datosA.timeout.connect(self.enviar_datos_timerA)
+        self.timer_envio_datosB = QTimer(self)
+        self.timer_envio_datosB.timeout.connect(self.enviar_datos_timerB)
         self.setGeometry(300, 300, 800, 600)
 
     def inicializar_ui(self):
-        self.layout_horizontal = QHBoxLayout()
+        self.layout_graficas = QHBoxLayout()
+        self.layout_slider_boton = QHBoxLayout()
+        self.layout_slider = QVBoxLayout()
+        self.layout_boton = QVBoxLayout()
+
 
         self.inicializar_grafica("A0")
         self.inicializar_grafica("A1")
 
-        self.layout.addLayout(self.layout_horizontal)
+        self.layout_principal.addLayout(self.layout_graficas)
 
         self.btn_conectar = QPushButton('Conectar', self)
         self.btn_conectar.clicked.connect(self.conectar_desconectar)
-        self.layout.addWidget(self.btn_conectar, alignment=Qt.AlignHCenter)
-        self.btn_conectar.setStyleSheet(f"background-color: {con}; border-radius: 10px; text-align: center;")
+        self.layout_boton.addWidget(self.btn_conectar, alignment=Qt.AlignCenter)
+        self.btn_conectar.setStyleSheet(f"background-color: {slateGray}; border-radius: 10px; text-align: center;")
         self.btn_conectar.setFixedSize(100, 30)
 
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setRange(0, 255)
-        self.slider.valueChanged.connect(self.controlar_led)
-        self.layout.addWidget(self.slider, alignment=Qt.AlignCenter)
+        self.btn_pausar = QPushButton('Pausar', self)
+        self.btn_pausar.clicked.connect(self.pausar_graficas)
+        self.layout_boton.addWidget(self.btn_pausar, alignment=Qt.AlignCenter)
+        self.btn_pausar.setStyleSheet(f"background-color: {slateGray}; border-radius: 10px; text-align: center;")
+        self.btn_pausar.setFixedSize(100, 30)
 
-        self.lbl_valor_slider = QLabel('Valor del Slider: 0', self)
-        self.layout.addWidget(self.lbl_valor_slider, alignment=Qt.AlignCenter)
+        self.slider_ledA = QSlider(Qt.Horizontal)
+        self.slider_ledA.setRange(0, 255)
+        self.slider_ledA.valueChanged.connect(self.control_ledA)
+        self.layout_slider.addWidget(self.slider_ledA, alignment=Qt.AlignCenter)
+
+        self.valor_sliderA = QLabel('Valor del Slider: 0', self)
+        self.layout_slider.addWidget(self.valor_sliderA, alignment=Qt.AlignCenter)
+
+        self.slider_ledB = QSlider(Qt.Horizontal)
+        self.slider_ledB.setRange(0, 255)
+        self.slider_ledB.valueChanged.connect(self.control_ledB)
+        self.layout_slider.addWidget(self.slider_ledB, alignment=Qt.AlignCenter)
+
+        self.valor_sliderB = QLabel('Valor del Otro LED: 0', self)
+        self.layout_slider.addWidget(self.valor_sliderB, alignment=Qt.AlignCenter)
+
+        self.layout_slider_boton.addLayout(self.layout_boton)
+        self.layout_slider_boton.addLayout(self.layout_slider)
+        self.layout_principal.addLayout(self.layout_slider_boton)
+
 
     def inicializar_grafica(self, nombre):
         canvas = FigureCanvas(plt.Figure())
-        self.layout_horizontal.addWidget(canvas)
+        self.layout_graficas.addWidget(canvas)
         ax = canvas.figure.add_subplot(111)
         line, = ax.plot([], [], label=nombre)
         ax.legend()
@@ -72,6 +104,9 @@ class Grafica(QWidget):
         self.conectado = not self.conectado
 
     def actualizar_graficas(self):
+        if not self.timer.isActive():
+            return
+
         datos_A0 = self.comunicacion.recibir_datos()
         datos_A1 = self.comunicacion.recibir_datos()
 
@@ -85,11 +120,10 @@ class Grafica(QWidget):
     def actualizar_grafica(self, nombre, valor):
         line = getattr(self, f"line_{nombre}")
         ax = getattr(self, f"ax_{nombre}")
+        max_puntos = 100
 
         x = list(line.get_xdata())
         y = list(line.get_ydata())
-
-        max_puntos = 100
 
         x.append(len(x))
         y.append(valor)
@@ -105,13 +139,40 @@ class Grafica(QWidget):
         canvas = getattr(self, f"canvas_{nombre}")
         canvas.draw()
 
-    def controlar_led(self, valor):
+
+
+    def pausar_graficas(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self.btn_pausar.setText('Reanudar')
+            self.btn_pausar.setStyleSheet(f"background-color: {steelBlue}; color:white; border-radius: 10px; text-align: center;")
+        else:
+            self.timer.start(100)
+            self.btn_pausar.setText('Pausar')
+            self.btn_pausar.setStyleSheet(f"background-color: {seaGreen}; color:white; border-radius: 10px; text-align: center;")
+
+
+
+    def control_ledA(self, valor):
         if isinstance(valor, (int, float)):
             valor = int(valor)
-            self.lbl_valor_slider.setText(f'Valor del Slider: {valor}')
-            self.timer_envio_datos.start(100)
+            self.valor_sliderA.setText(f'Valor del Slider: {valor}')
+            self.timer_envio_datosA.start(100)
 
-    def enviar_datos_desde_timer(self):
-        valor = self.slider.value()
-        self.comunicacion.enviar_datos(f'LED:{valor}')
-        self.timer_envio_datos.stop()
+    def enviar_datos_timerA(self):
+        valor = self.slider_ledA.value()
+        self.comunicacion.enviar_datos(f'LED1:{valor}\n')
+        self.timer_envio_datosA.stop()
+
+
+    def control_ledB(self, valor):
+        if isinstance(valor, (int, float)):
+            valor = int(valor)
+            self.valor_sliderB.setText(f'Valor del Otro LED: {valor}')
+            self.timer_envio_datosB.start(100)
+
+#enviar datos desde el timer para el led
+    def enviar_datos_timerB(self):
+        valor = self.slider_ledB.value()
+        self.comunicacion.enviar_datos(f'LED2:{valor}\n')
+        self.timer_envio_datosB.stop()
--- phyton/phyton.ino
+++ phyton/phyton.ino
@@ -1,7 +1,11 @@
 // Código del Arduino
+int ledPin1 = 9;  // Pin para el primer LED
+int ledPin2 = 10; // Pin para el segundo LED
+
 void setup() {
   Serial.begin(9600);
-  pinMode(9,OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
@@ -14,12 +18,15 @@ void loop() {
   Serial.println(valor_A1);
   delay(100);
 
-  if(Serial.available() > 0){
+  if (Serial.available() > 0) {
     String mensaje = Serial.readStringUntil('\n');
 
-    if(mensaje.startsWith("LED:")){
-      int valor_led = mensaje.substring(4).toInt();
-      analogWrite(9,valor_led);
+    if (mensaje.startsWith("LED1:")) {
+      int valor_led = mensaje.substring(5).toInt();
+      analogWrite(ledPin1, valor_led);
+    } else if (mensaje.startsWith("LED2:")) {
+      int valor_led = mensaje.substring(5).toInt();
+      analogWrite(ledPin2, valor_led);
     }
   }
 }
